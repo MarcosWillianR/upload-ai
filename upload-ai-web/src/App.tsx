cfb0a086539dedfadd54e438085db72da68f0a78
Marcos Willian <markusuuuu@gmail.com>
2023-09-16 16:05:15 -0300

    feature: finished Header UI

--- upload-ai-web/src/App.tsx
+++ upload-ai-web/src/App.tsx
@@ -1,10 +1,25 @@
+import { Github } from 'lucide-react';
+
 import { Button } from "@/components/ui/button";
+import { Separator } from '@/components/ui/separator';
 
 export function App() {
   return (
-    <div style={{ width: '100vw', textAlign: 'center' }}>
-      <h1>helowlrd</h1>
-      <Button>Click me</Button>
+    <div className="min-h-screen flex flex-col">
+      <div className="px-6 py-3 flex items-center justify-between border-b">
+        <h1 className="text-xl font-bold">upload.ai</h1>
+
+        <div className="flex items-center gap-3">
+          <span className="text-sm text-muted-foreground">Developed with ❤️ by Marcos</span>
+
+          <Separator orientation="vertical" className="h-6" />
+
+          <Button variant="outline">
+            <Github className="w-4 h-4 mr-2" />
+            Github
+          </Button>
+        </div>
+      </div>
     </div>
   )
 }
